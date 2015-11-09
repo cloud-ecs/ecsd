f4411312aa48f72deb1cce7998e1cc35fc73d711
Matt Simerson <matt@tnpi.net>
2015-11-09 09:32:50 -0800

    web UI progress

--- routes/scan.js
+++ routes/scan.js
@@ -4,7 +4,10 @@ var scanner = require('../lib/scanner');
 
 exports.public = function (app) {
 	// curl -X POST -F eicar=@eicar.eml localhost:8000/scan
-	
+
     app.post('/scan', scanner.post);
     app.get ('/scan', scanner.get);
+
+    app.get ('/status/scannersAll',       scanner.listAll);
+    app.get ('/status/scannersAvailable', scanner.listAvailable);
 };
