f5c1ac44db59cdbdfa5471e6e4c7a4e25cb7462d
Matt Simerson <matt@tnpi.net>
2015-11-14 17:59:26 -0800

    new base scanner class
    * spamassassin using base class
    * opendkim using base class
    * rspamd using HTTP protocol
    * update clamav & dspam
    * consistently return error objects

--- test/helpers/install-clamav.sh
+++ test/helpers/install-clamav.sh
@@ -3,7 +3,7 @@
 sudo apt-get install -y clamav clamav-base libclamav6 clamav-freshclam clamav-daemon
 
 # freshclam takes 50+ minutes to download databases
-sudo /usr/bin/freshclam
+#sudo /usr/bin/freshclam
 
 #sudo /etc/init.d/clamav-daemon start
 sudo invoke-rc.d clamav-daemon start
--- test/spamassassin.js
+++ test/spamassassin.js
@@ -3,7 +3,7 @@
 var assert = require('assert');
 var path   = require('path');
 
-var spam  = require('../lib/spamassassin');
+var spam  = require('../lib/spamassassin').createScanner();
 
 var spamMsg = path.resolve('test/files/gtube.eml');
 var hamMsg  = path.resolve('test/files/clean.eml');
@@ -18,120 +18,142 @@ before(function (done) {
     });
 });
 
-describe('spamassassin spamc', function () {
+describe('spamassassin', function () {
 
-    before(function (done) {
-        spam.binFound(function (err, bin) {
-            if (err) return done(err);
-            done();
+    describe('spamc cli', function () {
+
+        before(function (done) {
+            spam.binFound(function (err, bin) {
+                if (err) return done(err);
+                done();
+            });
+        });
+
+        it('finds gtube spam message', function (done) {
+            spam.scanBin(spamMsg, function (err, results) {
+                assert.ifError(err);
+                assert.equal(results.fail.length, 1);
+                done();
+            });
+        });
+
+        it('passes a clean message', function (done) {
+            spam.scanBin(hamMsg, function (err, results) {
+                assert.ifError(err);
+                // console.log(results);
+                assert.equal(results.pass.length, 1);
+                done();
+            });
         });
     });
 
-    it('finds gtube spam message', function (done) {
-        spam.scanBin(spamMsg, function (err, results) {
-            assert.ifError(err);
-            assert.equal(results.fail.length, 1);
-            done();
+    describe('TCP', function () {
+
+        before(function (done) {
+            spam.tcpListening(function (err, listening) {
+                if (err) return done(err);
+                done();
+            });
+        });
+
+        it('pings', function (done) {
+            spam.tcpAvailable(function (err, avail) {
+                assert.ifError(err);
+                assert.ok(avail);
+                done();
+            });
+        });
+
+        it('detects a spam message', function (done) {
+            spam.scanTcp(spamMsg, function (err, results) {
+                assert.ifError(err);
+                if (!results.fail.length) console.error(results);
+                assert.equal(results.fail.length, 1);
+                done();
+            });
+        });
+
+        it('passes a clean message', function (done) {
+            spam.scanTcp(hamMsg, function (err, results) {
+                // console.log(result);
+                assert.ifError(err);
+                assert.equal(results.pass.length, 1);
+                done();
+            });
         });
     });
 
-    it('passes a clean message', function (done) {
-        spam.scanBin(hamMsg, function (err, results) {
-            assert.ifError(err);
-            // console.log(results);
-            assert.equal(results.pass.length, 1);
-            done();
+    describe('unix socket', function () {
+
+        before(function (done) {
+            spam.socketFound(function (err, listening) {
+                if (err) return done(err);
+                done();
+            });
         });
-    });
-});
 
-describe('spamd TCP', function () {
-
-    before(function (done) {
-        spam.tcpListening(function (err, listening) {
-            if (err) return done(err);
-            done();
+        it('detects spam message', function (done) {
+            spam.scanSocket(spamMsg, function (err, results) {
+                assert.ifError(err);
+                // console.log(result);
+                assert.equal(results.fail.length, 1);
+                done();
+            });
         });
-    });
 
-    it('pings', function (done) {
-        spam.tcpAvailable(function (err, avail) {
-            assert.ifError(err);
-            assert.ok(avail);
-            done();
-        });
-    });
-
-    it('detects a spam message', function (done) {
-        spam.scanTcp(spamMsg, function (err, results) {
-            assert.ifError(err);
-            // console.log(result);
-            assert.equal(results.fail.length, 1);
-            done();
+        it('passes a clean message', function (done) {
+            spam.scanSocket(hamMsg, function (err, results) {
+                // console.log(result);
+                assert.ifError(err);
+                assert.equal(results.pass.length, 1);
+                done();
+            });
         });
     });
 
-    it('passes a clean message', function (done) {
-        spam.scanTcp(hamMsg, function (err, results) {
-            // console.log(result);
-            assert.ifError(err);
-            assert.equal(results.pass.length, 1);
-            done();
+    describe('scan dispatch', function () {
+
+        before(function (done) {
+            spam.isAvailable(function (err, available) {
+                if (err) return done(err);
+                if (!available) return done(new Error('spam not available'));
+                done();
+            });
         });
-    });
-});
 
-describe('spamd unix socket', function () {
-
-    before(function (done) {
-        spam.socketFound(function (err, listening) {
-            if (err) return done(err);
-            done();
+        it('scans spam', function (done) {
+            spam.scan(spamMsg, function (err, results) {
+                assert.ifError(err);
+                if (!results.fail.length) console.error(results);
+                assert.equal(results.fail.length, 1);
+                done();
+            });
         });
-    });
 
-    it('detects spam message', function (done) {
-        spam.scanSocket(spamMsg, function (err, results) {
-            assert.ifError(err);
-            // console.log(result);
-            assert.equal(results.fail.length, 1);
-            done();
+        it('scans clean', function (done) {
+            spam.scan(hamMsg, function (err, results) {
+                assert.ifError(err);
+                assert.equal(results.pass.length, 1);
+                done();
+            });
         });
-    });
 
-    it('passes a clean message', function (done) {
-        spam.scanSocket(hamMsg, function (err, results) {
-            // console.log(result);
-            assert.ifError(err);
-            assert.equal(results.pass.length, 1);
-            done();
+        it('scans spam concurrently', function (done) {
+            spam.scan(spamMsg, function (err, results) {
+                assert.ifError(err);
+                if (!results.fail.length) console.error(results);
+                assert.equal(results.fail.length, 1);
+                done();
+            });
         });
-    });
-});
 
-describe('spam scan dispatch', function () {
-
-    before(function (done) {
-        spam.isAvailable(function (err, available) {
-            if (err) return done(err);
-            if (!available) return done(new Error('spam not available'));
-            done();
+        it('scans clean concurrently', function (done) {
+            spam.scan(hamMsg, function (err, results) {
+                assert.ifError(err);
+                assert.equal(results.pass.length, 1);
+                done();
+            });
         });
-    });
 
-    it('scans spam', function (done) {
-        spam.scan(spamMsg, function (err, results) {
-            assert.ifError(err);
-            assert.equal(results.fail.length, 1);
-            done();
-        });
-    });
-
-    it('scans clean', function (done) {
-        spam.scan(hamMsg, function (err, results) {
-            assert.ifError(err);
-            assert.equal(results.pass.length, 1);
-            done();
-        });
     });
 });
