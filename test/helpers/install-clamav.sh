87e3d0568343b1c2d09c82660ee70df69e1a84f2
Matt Simerson <matt@tnpi.net>
2019-04-16 23:15:14 -0700

    CI test helpers

--- test/helpers/install-clamav.sh
+++ test/helpers/install-clamav.sh
@@ -3,12 +3,6 @@
 sudo apt-get install -y clamav clamav-base clamav-daemon libclamav7 clamav-freshclam
 
 # freshclam takes 50+ minutes to download databases
-#sudo /usr/bin/freshclam
+sudo /usr/bin/freshclam
 
-#
-#sudo touch /var/run/clamav/clamd.ctl
-#sudo chown clamav:clamav /var/run/clamav/clamd.ctl
-
-#sudo /etc/init.d/clamav-daemon start
-#sudo invoke-rc.d clamav-daemon start
-sudo service clamav-daemon start
+sudo invoke-rc.d clamav-daemon start
