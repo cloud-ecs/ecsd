0ca72af5a7aa724cb228ee04f1e07fa08bb532aa
Matt Simerson <matt@tnpi.net>
2019-04-16 22:44:09 -0700

    get clamav-daemon to start

--- test/helpers/install-clamav.sh
+++ test/helpers/install-clamav.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-sudo apt-get install -y clamav clamav-base libclamav6 clamav-freshclam clamav-daemon
+sudo apt-get install -y clamav clamav-base clamav-daemon libclamav7 clamav-freshclam
 
 # freshclam takes 50+ minutes to download databases
 #sudo /usr/bin/freshclam
 
+#
+#sudo touch /var/run/clamav/clamd.ctl
+#sudo chown clamav:clamav /var/run/clamav/clamd.ctl
+
 #sudo /etc/init.d/clamav-daemon start
-sudo invoke-rc.d clamav-daemon start
+#sudo invoke-rc.d clamav-daemon start
+sudo service clamav-daemon start
