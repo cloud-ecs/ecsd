66cbd49ba83f156f142d97e65ce0190ed3600ddd
Matt Simerson <matt@tnpi.net>
2019-04-18 01:53:04 -0700

    try starting daemons on Travis

--- test/helpers/install-clamav.sh
+++ test/helpers/install-clamav.sh
@@ -3,6 +3,4 @@
 sudo apt-get install -y clamav clamav-base clamav-daemon libclamav7 clamav-freshclam
 
 # freshclam takes 50+ minutes to download databases
-sudo /usr/bin/freshclam
-
-sudo invoke-rc.d clamav-daemon start
+sudo /usr/bin/freshclam && sudo invoke-rc.d clamav-daemon start
