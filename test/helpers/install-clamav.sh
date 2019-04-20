11f48c9d40d928ed6915ff198143401f8b4c5515
Matt Simerson <matt@tnpi.net>
2019-04-19 21:22:40 -0700

    more ci service startup work

--- test/helpers/install-clamav.sh
+++ test/helpers/install-clamav.sh
@@ -2,5 +2,5 @@
 
 sudo apt-get install -y clamav clamav-base clamav-daemon libclamav7 clamav-freshclam
 
-# freshclam takes 50+ minutes to download databases
-sudo /usr/bin/freshclam && sudo invoke-rc.d clamav-daemon start
+# freshclam can take 50+ minutes to download databases
+sudo /usr/bin/freshclam && sudo service clamav-daemon start
--- test/helpers/install-spamassassin.sh
+++ test/helpers/install-spamassassin.sh
@@ -2,4 +2,4 @@
 
 sudo apt-get install -y spamassassin
 sudo sed -i.bak -e 's/ENABLED.*/ENABLED=1/' /etc/default/spamassassin
-sudo sa-update && invoke-rc.d spamassassin start
+sudo sa-update && sudo service spamassassin start
